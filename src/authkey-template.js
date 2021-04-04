4d1bbeb627619f0716eb79953fbf48cc4f0bdb99
hermancai <hermancai3@gmail.com>
2021-04-04 06:36:06 -0700

    initial commit

---
+++ src/authkey-template.js
@@ -0,0 +1,6 @@
+/************************************
+ * Deepl API private key
+ * Replace x-y-z with own key
+ * Rename this file as "authkey.js"
+ ************************************/
+var authkey = "x-y-z";
