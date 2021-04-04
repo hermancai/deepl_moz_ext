9c04943a4d2e29520ced16bb3cf651276b489cbb
hermancai <hermancai3@gmail.com>
2021-04-04 01:20:38 -0700

    set default languages on startup

--- src/background.js
+++ src/background.js
@@ -6,12 +6,19 @@ browser.contextMenus.create({
 });
 
 
+// Set default language options
+let setDefaults = browser.storage.local.set({
+    "source": "",
+    "target": "EN-US"
+})
+
+
 // Run content script if clicked on extension
 browser.contextMenus.onClicked.addListener((info, tab) => {
     switch (info.menuItemId) {
       case 'deepl-translate':
         browser.tabs.executeScript({
-           file: "pageContentScript.js"
+           file: "contentScript.js"
         });
     }
 });
